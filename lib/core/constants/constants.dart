2aece043894358b81c1e7c253ad21e472d5536e9
Vince Umali <vince.umali@angkas.com>
2025-03-11 01:19:55 +0800

    fixed build_runner dependencies and finished API using retrofit

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = 'ac8276650fee4c75acd78e95ff1485bd';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
--- lib/core/resources/data_state.dart
+++ lib/core/resources/data_state.dart
@@ -2,15 +2,18 @@ import 'package:dio/dio.dart';
 
 abstract class DataState<T> {
   final T ? data;
-  final DioError ? error;
+  final DioException ? error;
+
+  // final DioError ? error;
 
   const DataState({this.data, this.error});
 }
 
+//when sucessful response
 class DataSuccess<T> extends DataState<T> {
   const DataSuccess(T data) : super(data: data);
 }
-
+//when failed response
 class DataFailed<T> extends DataState<T> {
-  const DataFailed(T data) : super(data: data);
+  const DataFailed(DioException error) : super(error: error);
 }
